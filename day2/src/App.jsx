c335e63b20baff8fdbdf3cebed8fedb4b38ca90a
Devesh <deveshgour6@gmail.com>
2026-09-03 00:16:04 +0530

    project of day 2

---
+++ day2/src/App.jsx
@@ -0,0 +1,76 @@
+
+
+const App = () => {
+  return (
+    <div>
+      <div className="main">
+        <div className="navbar">
+          <h2>Horizon courts</h2>
+          <div className="headings">
+            <p>About Us</p>
+            <p>Services</p>
+            <p>Coaches</p>
+            <p>Events</p>
+            <p>Contacts</p>
+          </div>
+          <button>Book now</button>
+        </div>
+        <div className="top">
+          <h3>Unleash Your Inner Champion Today. <br />All in One Place</h3>
+
+          <p>Join the ultimate tennis experience - where passion meets performance,<br />and every swings brings you closure to victory.</p>
+
+          <button>Start your own journey</button>
+        </div>
+        <div className="center">
+          <button>About Horizon</button>
+          <h4>At horizon courts, we believe in nurturing talent and fostering <br />a love for the game. Our experienced coaches are dedicated <br /> to helping you reach your full potential on the court.</h4>
+        </div>
+        <div className="cards">
+          <div className="card1">
+           <h4> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod ullam ratione asperiores fuga neque vero voluptates totam perferendis explicabo officia!</h4>
+          </div>
+          <div className="card2">
+            <p>Private & Group Lessons</p>
+          </div>
+          <div className="card3">
+            <div className="upper">
+              <h2>100+</h2>
+              <h5>Pro Coaches</h5>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, repellat.</p>
+            </div>
+            <div className="lower">
+              <h6>Beginner </h6>
+              <h6>Intermediate</h6>
+              <h6>Advanced</h6>
+            </div>
+          </div>
+        </div>
+            <div className="text">
+               <h4>A few more facts about us in numbers</h4>
+            </div>
+
+        <div className="footer">
+          <div className="data1">
+            <h2>12 000+</h2>
+            <p>Hours of play Anually</p>
+          </div>
+          <div className="data2">
+            <h2>89%</h2>
+            <p>Player Retention Rate</p>
+          </div>
+          <div className="data3">
+            <h2>1200+</h2>
+            <p>Active Members</p>
+          </div>
+          <div className="data1">
+            <h2>125+</h2>
+            <p>Annual Tournaments</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
