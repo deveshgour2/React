39308fe5e6a4ea9da3071063b520d11c7972b86f
Devesh <deveshgour6@gmail.com>
2026-09-09 02:19:59 +0530

    User card

--- user card/src/App.jsx
+++ user card/src/App.jsx
@@ -1,19 +1,24 @@
 import { useState } from "react"
-import Cards from "./components/Cards";
-
 
 const App = () => {
+
+  useState
   const [userName, setUserName] = useState('')
   const [userEmail, setUserEmail] = useState('')
   const [image, setImage] = useState('')
   const [role, setRole] = useState('')
-
-  const [allUser, setAllUser] = useState([])
+  
+  const localData = JSON.parse(localStorage.getItem('all-Users')) || []
+ 
+  const [allUser, setAllUser] = useState(localData)
 
   const submitHandler = (e) => {
     e.preventDefault()
 
-    setAllUser([...allUser, { userName, userEmail, image, role }])
+    const oldUser = [...allUser]
+    oldUser.push({ userName, userEmail, image, role })
+    setAllUser(oldUser)
+    localStorage.setItem('all-Users', JSON.stringify(oldUser))
 
     setUserName('')
     setImage('')
@@ -23,8 +28,18 @@ const App = () => {
 
   const deleteHandler = (idx) => {
     const copyUsers = [...allUser]
-    copyUsers.splice(idx, 1)
+  
+    const conf = confirm('Are you really want to Delete this element')
+
+    if(conf){
+        copyUsers.splice(idx, 1)
+    }
+    else{
+      alert('element not Deleted')
+    }
+
     setAllUser(copyUsers)
+    localStorage.setItem('all-Users', JSON.stringify(copyUsers))
   }
 
   return (
@@ -42,7 +57,7 @@ const App = () => {
             setUserName(e.target.value)
           }}
           className="border-2 outline-none px-5 py-2 mt-3 w-[48%] rounded font-semibold text-lg "
-           required
+          required
           type="text"
           placeholder="Enter your name" />
 
@@ -72,7 +87,7 @@ const App = () => {
             setRole(e.target.value)
           }}
           className="border-2 outline-none px-5  py-2 mt-3 w-[48%] rounded font-semibold text-lg"
-           required
+          required
           type="text"
           placeholder="Enter your Role" />
 
@@ -82,7 +97,20 @@ const App = () => {
       <div className="flex flex-wrap f-full">
 
         {allUser.map(function (elem, idx) {
-          return <Cards elem={elem} idx={idx} deleteHandler={deleteHandler}/>
+          return <div key={idx}>
+            <div className="h-60 w-55 bg-amber-50 m-4 rounded flex flex-col items-center justify-center flex-wrap gap-1 overflow-auto scrollbar-none">
+              <img className="h-20 w-20 rounded-full object-cover " src={elem.image} alt="" />
+              <h1 className="text-black font-bold text-xl capitalize leading-tight wrap-break-word">{elem.userName}</h1>
+              <h4 className="text-blue-600 font-semibold capitalize text-lg leading-tight wrap-break-word ">{elem.role}</h4>
+              <h6 className="text-black font-semibold text-sm leading-tight wrap-break-word">{elem.userEmail}</h6>
+
+              <button
+                onClick={() => {
+                  deleteHandler(idx)
+                }}
+                className="bg-red-500 px-5 py-1 rounded-2xl active:scale-95 font-medium mt-1"> Remove</button>
+            </div>
+          </div>
         })}
 
       </div>
